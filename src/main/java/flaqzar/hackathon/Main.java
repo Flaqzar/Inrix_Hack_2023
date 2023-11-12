b97a26c40998649e6a3a3f6849c1a3d43959dd5e
Flaqzar <51932016+Flaqzar@users.noreply.github.com>
2023-11-11 17:40:11 -0800

    testing

--- src/main/java/flaqzar/hackathon/Main.java
+++ src/main/java/flaqzar/hackathon/Main.java
@@ -1,9 +1,5 @@
 package flaqzar.hackathon;
 
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -16,24 +12,10 @@ public class Main extends Application
 	@Override
     public void start(Stage stage)
     {
-		String content = null;
-		URLConnection connection = null;
-		try {
-		  connection = new URL("https://en.wikipedia.org/wiki/Santa_Clara_County,_California").openConnection();
-		  Scanner scanner = new Scanner(connection.getInputStream());
-		  scanner.useDelimiter("\\Z");
-		  content = scanner.next();
-		  scanner.close();
-		}catch ( Exception ex ) {
-		    ex.printStackTrace();
-		}
-		
-		
-		System.out.println(content);
-		
-        stage.setTitle("Test");
-        this.scene = new Scene(new HTMLRegion("/testing.html"), 750, 500, Color.web("#666970"));
+		stage.setTitle("Test");
+        this.scene = new Scene(new HTMLRegion("/figma-to-html/index.html"), 750, 500, Color.web("#666970"));
         stage.setScene(this.scene);
+        //this.scene.getStylesheets().add("figma-to-html/css/main.css");
         stage.show();
     }
  
